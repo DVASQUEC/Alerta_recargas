2492e94dc1b19bd9ce98bfbbce1b600bc2ed68b6
DVASQUEC <dickladen02@gmail.com>
2026-07-31 00:17:40 -0500

    config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
   // Pega aquí la URL de tu implementación de Apps Script terminada en /exec
-  API_URL: 'https://script.google.com/macros/s/PEGA_AQUI_TU_DEPLOYMENT_ID/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycbw1QnCVp_tniHlFF9xED8aEZB-grtyail_vmhFqYIsG9oCD2Uc8LKly-1yxkrdclML5QQ/exec',
   APP_NAME: 'ALERTA RECARGAS'
 };
